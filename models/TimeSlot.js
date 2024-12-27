6ab7c597d0a831263914f7336f6fb3d838496985
vhmvdrezv <ahmdrzashn@gmail.com>
2024-12-27 22:15:21 +0330

    TimeSlot schema was created.

---
+++ models/TimeSlot.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const TimeSlotSchema = new Schema({
+    serviceProvider: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ServiceProvider',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['available', 'reserved', 'completed'],
+        default: 'available',
+    },
+    reservedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+TimeSlotSchema.index({ ServiceProvider: 1, date: 1});
+
+export default mongoose.model('TimeSlot', TimeSlotSchema);
