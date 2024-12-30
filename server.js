45dba8469644fbc3bf12237fda53cf53e1e06254
vhmvdrezv <ahmdrzashn@gmail.com>
2024-12-31 01:53:10 +0330

    #91 Global Error Handling Middleware

--- server.js
+++ server.js
@@ -72,12 +72,25 @@ app.use('/api/timeslots', TimeSlotRouter)
 
 app.use('/api/upload-article-image-body', uploadArticleImageBodyRouter);
 
-app.all("*", (req, res) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    });
-})
+app.all("*", (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message
+    })
+});
 
 mongoose.connection.once('open', () => {
     console.log('mongoDB connected');
