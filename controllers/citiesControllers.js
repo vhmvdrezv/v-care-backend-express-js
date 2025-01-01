3280c47f34d492680d18661a1ca3ca272c13bc50
vhmvdrezv <ahmdrzashn@gmail.com>
2025-01-01 15:33:24 +0330

    #96 Handling Invalid ID Error

--- controllers/citiesControllers.js
+++ controllers/citiesControllers.js
@@ -1,37 +1,32 @@
 import { logEvents } from "../middlewares/logEvents.js";
 import City from "../models/City.js";
+import asyncErrorHandler from "../utils/asyncErrorHandler.js";
 import { addCityValidator } from "../validators/citiesValidator.js";
 
-export const getAllCities = async (req, res) => {
-    try {
-        let cities;
-        if (['active', 'inactive'].includes(req?.query?.status)) {
-            cities = await City.find({ status: req?.query?.status });
-            return res.status(200).json({
-                message: "لیست شهرها:",
-                data: {
-                    cities
-                }
-            });
-        }
-    
-        cities = await City.find();
+export const getAllCities = asyncErrorHandler(async (req, res, next) => {
+
+    let cities;
+    if (['active', 'inactive'].includes(req?.query?.status)) {
+        cities = await City.find({ status: req?.query?.status });
         return res.status(200).json({
             message: "لیست شهرها:",
             data: {
                 cities
             }
         });
-    } catch (err) {
-        logEvents(err.message, 'errorLog.txt');
-        console.log(err.message);
-        res.status(500).json({
-            message: err.message
-        });
     }
-};
 
-export const addCityHandler = async (req, res) => {
+    cities = await City.find();
+    return res.status(200).json({
+        message: "لیست شهرها:",
+        data: {
+            cities
+        }
+    });
+
+});
+
+export const addCityHandler = asyncErrorHandler(async (req, res, next) => {
     const { error } = addCityValidator.validate(req.body);
     if (error) {
         return res.status(400).json({
@@ -39,30 +34,23 @@ export const addCityHandler = async (req, res) => {
         });
     }
 
-    try {
-        const cityNameExists = await City.findOne({ name: req.body.name });
-        if (cityNameExists) {
-            return res.status(409).json({
-                message: "اسم شهر موجود می باشد."
-            });
-        }
-
-        const city = await City.create({
-            name: req.body.name,
-            status: req.body.status
-        });
-
-        res.status(201).json({
-            message: "شهر اضافه شد.",
-            data: {
-                city
-            }
-        })
-    } catch (err) {
-        logEvents(err.message, 'errorLog.txt');
-        console.log(err.message);
-        res.status(500).json({
-            message: err.message
+    
+    const cityNameExists = await City.findOne({ name: req.body.name });
+    if (cityNameExists) {
+        return res.status(409).json({
+            message: "اسم شهر موجود می باشد."
         });
     }
-}
+
+    const city = await City.create({
+        name: req.body.name,
+        status: req.body.status
+    });
+
+    res.status(201).json({
+        message: "شهر اضافه شد.",
+        data: {
+        city
+        }
+    })
+});
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,38 @@
+const devErrors = (res, err) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        stackTrace: err.stack,
+        error: err
+    });
+}
+
+const prodErrors = (res, err) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong! Please try again later.'
+        });
+    }
+}
+
+
 export default  (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-        status: err.statusCode,
-        message: err.message
-    })
+
+    if (err.name === 'CastError') {
+        err = new CustomError(`Invalid ${err.path}: ${err.value}.`, 400);
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+        devErrors(res, err);
+    } else if (process.env.NODE_ENV === 'production') {
+        prodErrors(res, err);
+    }
 }; 
